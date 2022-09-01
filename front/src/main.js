9e6164c223d5d148a2076ab2828a12f9f33cabe6
phandyphorn <phornphandy20@gmail.com>
2022-09-01 09:45:30 +0700

    #18 crud student front

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 // import navAdmin from './components/admin/DashboardView.vue'
 import studentsListAdmin from './views/admin/StudentView.vue'
 import leaveListAdmin from './views/admin/LeaveView.vue'
 import deskbordListAdmin from './views/admin/DashboardView.vue'
 // import navAdmin from './components/admin/DashboardView.vue'
 import navStudents from './components/student/StudentNavbar.vue'
+import AddStudentForm from './components/student/AddStudentForm.vue'
+import EditStudentForm from './components/student/EditStudentForm'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
@@ -17,6 +20,9 @@ app.component('StudentViewAdmin',studentsListAdmin)
 app.component('LeavetViewAdmin',leaveListAdmin)
 app.component('DeskbordViewAdmin',deskbordListAdmin)
 app.component('navStudent',navStudents)
+app.component('addStudent', AddStudentForm)
+app.component('editStudent', EditStudentForm)
+
 
 
 app.use(router)
